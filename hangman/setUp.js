7ad30592e1ddd43887bdf0ddfe14a95bc61d7ab5
Chris Hildebrand <hildebrandc94@gmail.com>
2023-06-16 20:02:46 -0400

    basic set up with word bank and asci art

---
+++ hangman/setUp.js
@@ -0,0 +1,67 @@
+const wordBank = ["hangman", "javascript", "programming", "terminal", "computer", "code", "developer", "game", "challenge", "learning", "algorithm", "function", "array", "variable", "loop", "condition", "string", "object", "boolean"];
+
+const hangmanPics = [
+  `
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========`,
+  `
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========`,
+  `
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========`,
+  `
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========`,
+];
+
+console.log(hangmanPics[5]);
+
+module.exports = {
+  wordBank,
+  hangmanPics,
+};
