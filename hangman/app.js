e5681c69c6c41470fc8273e625c354554a4cf2a6
Chris Hildebrand <hildebrandc94@gmail.com>
2023-06-18 05:33:06 -0400

    Finished Hangman

--- hangman/app.js
+++ hangman/app.js
@@ -1,4 +1,3 @@
-// import { hangmanPics, wordBank } from "./setUp.js";
 const { wordBank, hangmanPics, asciiArt } = require("./setUp.js");
 const prompt = require("prompt-sync")();
 
@@ -7,30 +6,60 @@ const randomWord = () => {
   const index = Math.floor(Math.random() * wordBank.length);
   return wordBank[index].split("");
 };
-// let guessedArray = (word) => new Array(word.length).fill("_");
 
-const checkExit = (word) => (word === quit ? (gameActive = false) : (gameActive = true));
+const checkExit = (int) => (int === "quit" ? (gameActive = false) : (gameActive = true));
 
+const checkLoss = (attempts) => {
+  if (attempts === 6) {
+    console.log("To many attempts better luck next time");
+    gameActive = false;
+  }
+};
+const checkWin = (wordPlaceHolder, word) => {
+  if (wordPlaceHolder.join("") === word.join("")) {
+    console.log(`${word.join("")} is correct`);
+    gameActive = false;
+  }
+};
+
+// Intro art
 console.log(asciiArt);
 
+//Game variables
 let gameActive = true;
 const word = randomWord();
-console.log(word);
+const wordPlaceHolder = new Array(word.length).fill("_");
+const history = [];
 let attempts = 0;
 
+// Uncomment for testing to show the word
+// console.log(word.join(""));
+
+// main game loop
 while (gameActive) {
   let guess = prompt("Guess a letter: ");
+  //Check if player wants to quit
   checkExit(guess);
+  //Show player all of their past guess's
+  history.push(guess);
+  console.log(`You have guessed ${history}`);
+
   if (word.includes(guess)) {
-    let guessedArray = word.filter((letter) => (letter === guess ? letter : "_"));
-    console.log(guessedArray);
+    word.forEach((letter, index) => {
+      if (letter === guess) {
+        wordPlaceHolder[index] = letter;
+      }
+    });
+    // Show all correct guessed letter in word
+    console.log(wordPlaceHolder.join(" "));
+    // Check for win
+    checkWin(wordPlaceHolder, word);
+    // Show current amount of wrong attempts through hangman asci art
     console.log(hangmanPics[attempts]);
   } else {
+    // If guess is wrong showing the next asci art and checking for loss
     attempts += 1;
     console.log(hangmanPics[attempts]);
-    if (attempts === 6) {
-      console.log("You Lose");
-      gameActive = false;
-    }
+    checkLoss(attempts);
   }
 }
