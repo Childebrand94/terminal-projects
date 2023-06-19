397f1be177ad6861b3c5bb1d18f92d829a1ef2a3
Chris Hildebrand <hildebrandc94@gmail.com>
2023-06-18 20:18:29 -0400

    working on line 12, the password is only returning one element

--- random-password-generator/app.js
+++ random-password-generator/app.js
@@ -1,5 +1,20 @@
+const prompt = require("prompt-sync")();
+
 const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "\\", ";", ":", "'", '"', ",", ".", "<", ">", "/", "?", "~", "`"];
 const alphabeticalCharacters = [...Array(26)].map((_, i) => String.fromCharCode(97 + i));
 const numbers = [...Array(10)].map((_, i) => i.toString());
+const passwordCharacters = [specialCharacters, alphabeticalCharacters, numbers];
+const randomIndex = (max) => Math.floor(Math.random() * max);
 
-console.log(numbers);
+const length = prompt("How many characters is the password? : ");
+
+const passwordGen = (length) => {
+  const password = [...Array(length)].map((_) => {
+    let listChoice = passwordCharacters[randomIndex(passwordCharacters.length)];
+    console.log(listChoice);
+    return listChoice[randomIndex(listChoice.length)];
+  });
+  return password.join("");
+};
+
+console.log(passwordGen(length));
