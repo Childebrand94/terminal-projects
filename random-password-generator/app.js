7b753eeb9356fd413a29cd6cee0698222ec56eb1
Chris Hildebrand <hildebrandc94@gmail.com>
2023-06-19 05:11:46 -0400

    Added comments

--- random-password-generator/app.js
+++ random-password-generator/app.js
@@ -1,16 +1,19 @@
 const prompt = require("prompt-sync")();
 
+// Lists of characters
 const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "\\", ";", ":", "'", '"', ",", ".", "<", ">", "/", "?", "~", "`"];
 const alphabeticalCharacters = [...Array(26)].map((_, i) => String.fromCharCode(97 + i));
 const upperAlphabeticalCharacters = [...Array(26)].map((_, i) => String.fromCharCode(65 + i));
 const numbers = [...Array(10)].map((_, i) => i.toString());
 const randomIndex = (limit) => Math.floor(Math.random() * limit);
 
+// User input
 const n = prompt("How many characters is the password? : ");
 console.log(n);
 
+// Main function
 const passwordGen = (length) => {
-  let password = [...Array(parseInt(length))].map(() => {
+  let password = [...Array(parseInt(length))].map((_) => {
     const randomNumber = randomIndex(10);
     if (randomNumber >= 8) {
       return alphabeticalCharacters[randomIndex(26)];
