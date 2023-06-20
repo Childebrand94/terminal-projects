6ff476e59be7851065e229316b2cb894e84458ab
Chris Hildebrand <hildebrandc94@gmail.com>
2023-06-19 20:00:37 -0400

    learning how to use Sharp

--- image-manipulator/app.js
+++ image-manipulator/app.js
@@ -1,16 +1,16 @@
 const sharp = require("sharp");
 const prompt = require("prompt-sync")();
 
-const file = prompt("Please enter the file path: ");
+// const file = prompt("Please enter the file path: ");
 
 const getMetaData = async () => {
   try {
-    const metaData = sharp(file).metaData();
-    return metaData;
+    const metaData = await sharp("images/underwater.png").metadata();
+    console.log(metaData);
   } catch (err) {
     console.log("Something went wrong");
     console.log(err);
   }
 };
 
-getMetaData();
+const output = await sharp("images/underwater.png").greyscale().toBuffer().toFile("image-manipulator");
